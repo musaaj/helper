78797b906f52b2362cc05275e2746f71d274b0ad
Musa Ibrahim <musaaj@gmail.com>
2023-02-11 10:23:04 +0100

    Auto Commit

--- line_parser.py
+++ line_parser.py
@@ -5,29 +5,31 @@ def parse_line(line):
         line: str
 
     Description:
-        this function splits a given string by space characters or '"'. Characters withing 2 '"' are considered single tokens
+        this function splits a given string by space characters or 
+        '"'. Characters withing 2 '"' are considered single tokens
 
     Returns: list of strings
     """
     if line and isinstance(line, str):
         line = line.strip()
+        length = len(line)
         token = '';
         result = []
         i = 0;
-        while i < len(line):
+        while i < length:
             if line[i] == ' ':
                 i += 1
                 continue
             if line[i] in '"':
                 i += 1
-                while i < len(line) and line[i] != '"':
+                while i < length and line[i] != '"':
                     token += line[i]
                     i += 1
                 result.append(token)
                 token = ''
                 i += 1
-            elif i < len(line):
-                while i < len(line) and line[i] != ' ':
+            elif i < length:
+                while i < length and line[i] != ' ':
                     token += line[i]
                     i += 1
                 result.append(token)
